bc549929813ca56b83bb8cb3906fe42455c83f34
Guhan N S <guhan.neelakandan@srmtech.com>
2025-11-28 10:30:53 +0530

    changes in theme

--- src/Themecontext.jsx
+++ src/Themecontext.jsx
@@ -1,29 +1,26 @@
 import { createContext, useContext, useEffect, useState } from "react";
- 
+
 const ThemeContext = createContext();
- 
+
 export const useTheme = () => useContext(ThemeContext);
- 
+
 export const ThemeProvider = ({ children }) => {
-  const [isDarkMode, setIsDarkMode] = useState(() => {
-    return localStorage.getItem("theme") === "Dark"; // store match check
-  });
- 
+  const [isDarkMode, setIsDarkMode] = useState(false); // default Light
+
   const toogleTheme = () => {
-    setIsDarkMode((prevState) => !prevState);
+    setIsDarkMode(prev => !prev);
   };
- 
+
   const theme = isDarkMode ? "Dark" : "Light";
- 
+
   useEffect(() => {
     document.documentElement.setAttribute("data-theme", theme);
     localStorage.setItem("theme", theme);
   }, [isDarkMode, theme]);
- 
+
   return (
     <ThemeContext.Provider value={{ theme, toogleTheme }}>
       {children}
     </ThemeContext.Provider>
   );
 };
- 
